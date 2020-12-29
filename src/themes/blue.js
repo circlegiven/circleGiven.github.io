ef643dd895c38750fc1631403236ad4f45f5e156
circlegiven <circlegiven@gmail.com>
2020-12-29 20:59:37 +0900

    blue 테마 작업중

---
+++ src/themes/blue.js
@@ -0,0 +1,62 @@
+export default {
+    main: {
+        defaultBack: "#fff",
+        defaultText: "#000",
+        overlayBack: "#000",
+        header: {
+            back: "#3f0f3f",
+            text: "#fff",
+            button: "#fff"
+        }
+    },
+    side: {
+        defaultBack: "#2198F3",
+        defaultText: "#eee",
+        bio: {
+            circle: "#00ff03",
+            defaultText: "#d0d0d0",
+            activeText: "#eee"
+        },
+        category: {
+            activeText: "#000",
+            activeBack: "#eee"
+        }
+    },
+    bio: {
+        defaultBack: "#eee",
+        defaultText: "#000",
+        subText: "#4d4d4d",
+        socialButton: "#000"
+    },
+    postlistitem: {
+        title: "#000",
+        content: "#999",
+        info: "#000"
+    },
+    pagination: {
+        defaultText: "#aaa",
+        activeText: "#eee",
+        activeBack: "#212121"
+    },
+    blogpost: {
+        title: "#000",
+        info: "#999",
+        hr: "#999",
+        content: {
+            default: "#000",
+            quote: "#999",
+            link: "#0687f0"
+        }
+    },
+    tag: {
+        back: "#d9d9d9",
+        text: "#4d4d4d"
+    },
+    recentpostlist: {
+        header: "#eee",
+        category: "#0687f0"
+    },
+    recentpostitem: {
+        text: "#eee"
+    }
+};
